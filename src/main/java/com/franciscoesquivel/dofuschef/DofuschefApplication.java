eacea3bc92f8dbf40e6b282904b093c8ecd2826a
Francisco Esquivel Rico <franciscoesquivel032@gmail.com>
2026-05-26 11:16:28 +0200

    Init

---
+++ src/main/java/com/franciscoesquivel/dofuschef/DofuschefApplication.java
@@ -0,0 +1,14 @@
+package com.franciscoesquivel.dofuschef;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DofuschefApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DofuschefApplication.class, args);
+		System.out.println("Hola mundo!");
+	}
+
+}
---
+++ src/test/java/com/franciscoesquivel/dofuschef/DofuschefApplicationTests.java
@@ -0,0 +1,13 @@
+package com.franciscoesquivel.dofuschef;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DofuschefApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
