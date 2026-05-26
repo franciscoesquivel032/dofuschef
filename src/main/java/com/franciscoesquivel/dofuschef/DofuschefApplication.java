6a847b47cbe49dc2e5d4371ae881ff56082ba204
Francisco Esquivel Rico <franciscoesquivel032@gmail.com>
2026-05-26 13:59:44 +0200

    feat: dofusdude API maven integration

--- src/main/java/com/franciscoesquivel/dofuschef/DofuschefApplication.java
+++ src/main/java/com/franciscoesquivel/dofuschef/DofuschefApplication.java
@@ -1,5 +1,9 @@
 package com.franciscoesquivel.dofuschef;
 
+import com.dofusdude.client.ApiClient;
+import com.dofusdude.client.ApiException;
+import com.dofusdude.client.Configuration;
+import com.dofusdude.client.api.EquipmentApi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,7 +12,19 @@ public class DofuschefApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DofuschefApplication.class, args);
-		System.out.println("Hola mundo!");
+		ApiClient client = Configuration.getDefaultApiClient();
+		client.setBasePath("https://api.dofusdu.de");
+
+		EquipmentApi api = new EquipmentApi(client);
+		String language, gameName;
+		language = "es";
+		gameName = "dofus3";
+		try{
+			System.out.println(api.getItemsEquipmentSearch(language, gameName, "guerra", 0, 200, 8, null));
+		}catch(ApiException e){
+			System.out.println("Error " + e);
+		}
+
 	}
 
 }
